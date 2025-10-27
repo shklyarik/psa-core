1df629848f9a2ae5f46c1510a23db69a12942be0
Pavel <dementorpasha@gmail.com>
2025-10-27 23:21:14 +0300

    Improve help cli command

--- src/Cli/Commands/HelpCommand.php
+++ src/Cli/Commands/HelpCommand.php
@@ -8,6 +8,36 @@ class HelpCommand
 {
     public function run(App $app)
     {
-        print_r($app->dispatcher->getCommands());
+        $commands = $app->dispatcher->getCommands();
+
+        $groups = [];
+        $standalone = [];
+
+        foreach ($commands as $command => $action) {
+            if ($command === 'help') {
+                continue;
+            }
+
+            if (strpos($command, ':') !== false) {
+                [$group, $sub] = explode(':', $command, 2);
+                $groups[$group][] = $sub;
+            } else {
+                $standalone[] = $command;
+            }
+        }
+
+        foreach ($standalone as $cmd) {
+            echo $cmd . "\n";
+        }
+
+        echo "\n";
+
+        foreach ($groups as $group => $subcommands) {
+            echo $group . ":\n";
+            foreach ($subcommands as $sub) {
+                echo "  - " . $group . ":" . $sub . "\n";
+            }
+            echo "\n";
+        }
     }
 }
